98ed0542adf7e77ae63a3e99a446f5567cc151e6
jwmoss <jwmoss88@gmail.com>
2020-12-18 17:05:22 -0500

    update

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 docker build . -t pac:1.0
 
 ## run
-docker run -it -v /Users/jmoss/Documents/plex-auto-collections:/config --name pac pac:1.0 -u
+docker run -it -v /Users/jmoss/Plex-Auto-Collections/config:/config --name pac pac:1.0 -u
